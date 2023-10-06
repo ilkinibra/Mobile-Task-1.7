8195dff28d723de90460cffcb55d063bf205747f
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2023-10-06 14:01:25 +0400

    Add files via upload

---
+++ Task 1.7/index.js
@@ -0,0 +1,13 @@
+let num =9999;
+
+if(num<100 && num>=10){
+    console.log("iki reqemli ededdir")
+}
+else if(num <10 && num >=1){
+    console.log("bir reqemli ededdir")
+}else if(num <1000 && num >=100){
+    console.log("uc reqemli ededdir")
+}
+else{
+    console.log("max oxuma heddini kecib")
+}
